7d53438c8d3101598ebb8eadf187423e9ef58f79
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-06 20:28:07 +0200

    Rename plugin to build failure resolution assistant and show name on login screen

--- intellij-build-failure-resolution-assistant/src/main/java/ch/scheitlin/alex/intellij/plugins/toolWindow/LoginPanel.java
+++ intellij-build-failure-resolution-assistant/src/main/java/ch/scheitlin/alex/intellij/plugins/toolWindow/LoginPanel.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 
 public class LoginPanel extends JPanel {
     private JPanel panelContent;
+    private JLabel labelTitle;
     private JLabel labelInformation;
     private JButton buttonLogin;
     private JLabel labelIcon;
@@ -28,30 +29,37 @@ public class LoginPanel extends JPanel {
         // configure and add content panel
         initAndAddContentPanel(20);
 
+        // configure and add label with title
+        initTitleLabel();
+        c.gridx = 0;
+        c.gridy = 0;
+        this.panelContent.add(labelTitle, c);
+
         // configure and add label with information message
         initInformationLabel();
         c.gridx = 0;
-        c.gridy = 0;
+        c.gridy = 1;
+        c.insets = JBUI.insets(20, 0, 0, 0);
         this.panelContent.add(labelInformation, c);
 
         // configure and add button to login
         initLoginButton();
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         c.insets = JBUI.insets(20, 0, 0, 0);
         this.panelContent.add(buttonLogin, c);
 
         // configure and add label with icon
         initIconLabel();
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         c.insets = JBUI.insets(50, 0, 0, 0);
         this.panelContent.add(this.labelIcon, c);
 
         // add panel to move content to the top
         // (at least one component needs to have weighty greater than 0.0)
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 4;
         c.fill = GridBagConstraints.BOTH;
         c.insets = JBUI.insets(0);
         c.weightx = 1.0;
@@ -72,6 +80,15 @@ public class LoginPanel extends JPanel {
         c.weightx = 1.0;
         c.weighty = 1.0;
         this.add(this.panelContent, c);
+    }
+
+    private void initTitleLabel() {
+        this.labelTitle = new JLabel();
+        this.labelTitle.setText("Build Failure Resolution Assistant");
+
+        Font labelFont = this.labelTitle.getFont();
+        Font newFont = new Font(labelFont.getName(), Font.BOLD, 15);
+        this.labelTitle.setFont(newFont);
     }
 
     private void initInformationLabel() {
