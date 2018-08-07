136acdc04a8ff97619f36e0df74962b482ee1f6e
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-07 18:08:08 +0200

    Add scroll pane to maven build tree view

--- intellij-build-failure-resolution-assistant/src/main/java/ch/scheitlin/alex/intellij/plugins/toolWindow/TreeViewPanel.java
+++ intellij-build-failure-resolution-assistant/src/main/java/ch/scheitlin/alex/intellij/plugins/toolWindow/TreeViewPanel.java
@@ -2,26 +2,32 @@ package ch.scheitlin.alex.intellij.plugins.toolWindow;
 
 import ch.scheitlin.alex.maven.MavenBuild;
 import ch.scheitlin.alex.maven.MavenPanel;
+import com.intellij.ui.components.JBScrollPane;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
 
 public class TreeViewPanel extends JPanel {
+    private JBScrollPane scrollPane;
     private MavenPanel mavenPanel;
 
     public TreeViewPanel (MavenBuild mavenBuild) {
         this.mavenPanel = new MavenPanel(mavenBuild);
 
+        // create scroll pane with tree
+        this.scrollPane = new JBScrollPane(this.mavenPanel);
+        this.scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
         // set layout
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        // add tree
+        // add scroll pane
         c.fill = GridBagConstraints.BOTH;
         c.gridx = 0;
         c.gridy = 0;
         c.weightx = 1.0;
         c.weighty = 1.0;
-        this.add(this.mavenPanel, c);
+        this.add(this.scrollPane, c);
     }
 }
