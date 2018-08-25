bd3ed330468ea7d00486c28f47fd434e84510778
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-26 00:08:27 +0200

    Clean up controller

--- caesar-intellij-plugin/src/main/java/ch/scheitlin/alex/intellij/plugins/services/Controller.java
+++ caesar-intellij-plugin/src/main/java/ch/scheitlin/alex/intellij/plugins/services/Controller.java
@@ -3,6 +3,7 @@ package ch.scheitlin.alex.intellij.plugins.services;
 import ch.scheitlin.alex.build.Caesar;
 import ch.scheitlin.alex.build.model.BuildServer;
 import ch.scheitlin.alex.build.model.BuildServerBuild;
+import ch.scheitlin.alex.build.model.BuildServerProject;
 import ch.scheitlin.alex.build.model.BuildServerType;
 import ch.scheitlin.alex.build.model.Error;
 import ch.scheitlin.alex.intellij.plugins.dialogs.LoginDialog;
@@ -14,18 +15,15 @@ import ch.scheitlin.alex.maven.MavenGoal;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
 import javafx.util.Pair;
 
-import java.io.File;
 import java.util.List;
 
 public class Controller {
     private Storage storage;
     private Caesar caesar;
+
     private Project project;
     private ToolWindow toolWindow;
     private String buildServerProjectName;
@@ -34,7 +32,7 @@ public class Controller {
     private final BuildServerType BUILD_SERVER_TYPE = BuildServerType.TEAM_CITY;
 
     public Controller() {
-        this.storage = ServiceManager.getService(Storage.class);
+        this.storage = Storage.getInstance();
         this.caesar = new Caesar(this.BUILD_SERVER_TYPE);
     }
 
@@ -42,22 +40,35 @@ public class Controller {
         return ServiceManager.getService(Controller.class);
     }
 
+    public boolean testBuildServerConnection(String host, String username, String password) {
+        return this.caesar.testBuildServerConnection(this.BUILD_SERVER_TYPE, host, username, password);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // CAESAR: connect
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // try to login without asking for credentials
     public boolean tryAutoLogin(Project project) {
         return login(project, false);
     }
 
+    // login with asking for credentials
     public boolean login(Project project) {
         return login(project, true);
     }
 
+    // login with saved or provided credentials
     private boolean login(Project project, boolean openDialog) {
         this.project = project;
 
+        // get credentials from storage (if the are available)
         List<Pair<String, String>> properties = this.storage.getBuildServerCredentials();
         String host = properties.get(0).getValue();
         String username = properties.get(1).getValue();
         String password = properties.get(2).getValue();
 
+        // ask for credentials
         if (openDialog) {
             // show login dialog to get build server host, username, and password
             LoginDialog dialog = new LoginDialog();
@@ -89,25 +100,6 @@ public class Controller {
         return false;
     }
 
-    public void logout() {
-        this.caesar.disconnect();
-
-        // update tool window
-        this.getToolWindow().update();
-    }
-
-    public void setToolWindow(ToolWindow toolWindow) {
-        this.toolWindow = toolWindow;
-    }
-
-    public ToolWindow getToolWindow() {
-        return this.toolWindow;
-    }
-
-    public void showToolWindow() {
-        IntelliJHelper.showToolWindow(this.project, "BFR Assistant");
-    }
-
     public BuildServer fetchBuildServerInformation() {
         return this.caesar.fetchBuildServerInformation();
     }
@@ -116,32 +108,8 @@ public class Controller {
         return this.caesar.getBuildServerInformation().getProjectNames();
     }
 
-    public ch.scheitlin.alex.build.model.BuildServerProject getBuildServerProject(String projectName) {
+    public BuildServerProject getBuildServerProject(String projectName) {
         return this.caesar.getBuildServerInformation().getProject(projectName);
-    }
-
-    public String getGitRepositoryOriginUrl() {
-        return IntelliJHelper.getProjectPath(this.project);
-    }
-
-    public boolean isInNoStage() {
-        return this.caesar.isInNoStage();
-    }
-
-    public boolean isConnected() {
-        return this.caesar.isConnected();
-    }
-
-    public boolean hasDownloaded() {
-        return this.caesar.hasDownloaded();
-    }
-
-    public boolean hasProcessed() {
-        return this.caesar.hasProcessed();
-    }
-
-    public boolean isFixing() {
-        return this.caesar.isFixing();
     }
 
     public void saveBuildServerCredentials(String host, String username, String password) {
@@ -152,9 +120,9 @@ public class Controller {
         this.storage.deleteBuildServerCredentials();
     }
 
-    public boolean testBuildServerConnection(String host, String username, String password) {
-        return this.caesar.testBuildServerConnection(this.BUILD_SERVER_TYPE, host, username, password);
-    }
+    // -----------------------------------------------------------------------------------------------------------------
+    // CAESAR: download & process
+    // -----------------------------------------------------------------------------------------------------------------
 
     public void getBuildInformation(String buildConfigurationName, BuildServerBuild build) {
         this.buildServerConfigurationName = buildConfigurationName;
@@ -170,89 +138,14 @@ public class Controller {
         updateToolWindow();
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+    // CAESAR: fix
+    // -----------------------------------------------------------------------------------------------------------------
+
     public void startFixingBrokenBuild() {
-        this.caesar.fix(this.getGitRepositoryOriginUrl());
-        reloadProjectFiles();
+        this.caesar.fix(IntelliJHelper.getProjectPath(this.project));
+        IntelliJHelper.reloadProjectFiles(this.project);
         updateToolWindow();
-    }
-
-    public void stopFixingBrokenBuild() {
-        this.caesar.finish();
-        reloadProjectFiles();
-        updateToolWindow();
-    }
-
-    public void abortBuildFix() {
-        this.caesar.abort();
-
-        updateToolWindow();
-    }
-
-    public String getBuildServerProjectName() {
-        return this.buildServerProjectName;
-    }
-
-    public void setBuildServerProjectName(String projectName) {
-        this.buildServerProjectName = projectName;
-    }
-
-    public String getBuildServerBuildConfigurationName() {
-        return this.buildServerConfigurationName;
-    }
-
-    public String getBuildStatus() {
-        return this.caesar.getMavenBuild().getStatus().toString();
-    }
-
-    public String getFailedGoal() {
-        if (this.caesar.getMavenBuild().getFailedGoal() != null) {
-            MavenGoal failedGoal = this.caesar.getMavenBuild().getFailedGoal();
-            return failedGoal.getPlugin().getName() + ":" + failedGoal.getPlugin().getVersion() + ":" + failedGoal.getName();
-        } else {
-            return "No failed goal detected!";
-        }
-    }
-
-    public String getFailureCategory() {
-        if (this.caesar.getFailureCategory() != null) {
-            return this.caesar.getFailureCategory();
-        } else {
-            return "No category found!";
-        }
-    }
-
-    public List<Error> getErrors() {
-        return this.caesar.getErrors();
-    }
-
-    public Project getIntelliJProject() {
-        return this.project;
-    }
-
-    private void updateToolWindow() {
-        this.toolWindow.update();
-    }
-
-    private void reloadProjectFiles() {
-        IntelliJHelper.getProjectDirectoryFile(this.project).refresh(true, true);
-    }
-
-    public MavenBuild getMavenBuild() {
-        return this.caesar.getMavenBuild();
-    }
-
-    public String getBuildServerBuildLog() {
-        return this.caesar.getBuildServerBuildLog();
-    }
-
-    public void openErrorInFile(Error error) {
-        String filePath = IntelliJHelper.getProjectPath(this.project) + "/" + error.getFullPath();
-
-        int lineNumber = error.getLine() - 1;
-        int columnNumber = error.getColumn() - 1;
-
-        // open file
-        openFile(filePath, lineNumber, columnNumber);
     }
 
     public void debugError(Error error) {
@@ -281,12 +174,135 @@ public class Controller {
         }
     }
 
-    private void openFile(String filePath, int lineNumber, int columnNumber) {
-        File file = new File(filePath);
-        VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(file);
-        OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(this.project, virtualFile, lineNumber, columnNumber);
-        openFileDescriptor.navigate(true);
+    public void openErrorInFile(Error error) {
+        String filePath = IntelliJHelper.getProjectPath(this.project) + "/" + error.getFullPath();
 
-        System.out.println("Opening " + IntelliJHelper.getProjectPath(this.project) + "/" + filePath);
+        int lineNumber = error.getLine() - 1;
+        int columnNumber = error.getColumn() - 1;
+
+        // open file
+        IntelliJHelper.openFile(this.project, filePath, lineNumber, columnNumber);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // CAESAR: finish
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public void stopFixingBrokenBuild() {
+        this.caesar.finish();
+        IntelliJHelper.reloadProjectFiles(this.project);
+        updateToolWindow();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // CAESAR: disconnect
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public void logout() {
+        this.caesar.disconnect();
+
+        // update tool window
+        this.getToolWindow().update();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // CAESAR: abort
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public void abortBuildFix() {
+        this.caesar.abort();
+
+        updateToolWindow();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // CAESAR: stages
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public boolean isInNoStage() {
+        return this.caesar.isInNoStage();
+    }
+
+    public boolean isConnected() {
+        return this.caesar.isConnected();
+    }
+
+    public boolean hasDownloaded() {
+        return this.caesar.hasDownloaded();
+    }
+
+    public boolean hasProcessed() {
+        return this.caesar.hasProcessed();
+    }
+
+    public boolean isFixing() {
+        return this.caesar.isFixing();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public Project getIntelliJProject() {
+        return this.project;
+    }
+
+    public ToolWindow getToolWindow() {
+        return this.toolWindow;
+    }
+
+    public void setToolWindow(ToolWindow toolWindow) {
+        this.toolWindow = toolWindow;
+    }
+
+    private void updateToolWindow() {
+        this.toolWindow.update();
+    }
+
+    public void showToolWindow() {
+        IntelliJHelper.showToolWindow(this.project, "BFR Assistant");
+    }
+
+    public String getBuildServerProjectName() {
+        return this.buildServerProjectName;
+    }
+
+    public void setBuildServerProjectName(String projectName) {
+        this.buildServerProjectName = projectName;
+    }
+
+    public String getBuildServerBuildConfigurationName() {
+        return this.buildServerConfigurationName;
+    }
+
+    public String getBuildServerBuildLog() {
+        return this.caesar.getBuildServerBuildLog();
+    }
+
+    public MavenBuild getMavenBuild() {
+        return this.caesar.getMavenBuild();
+    }
+
+    public String getBuildStatus() {
+        return this.caesar.getMavenBuild().getStatus().toString();
+    }
+
+    public String getFailedGoal() {
+        if (this.caesar.getMavenBuild().getFailedGoal() != null) {
+            MavenGoal failedGoal = this.caesar.getMavenBuild().getFailedGoal();
+            return failedGoal.getPlugin().getName() + ":" + failedGoal.getPlugin().getVersion() + ":" + failedGoal.getName();
+        } else {
+            return "No failed goal detected!";
+        }
+    }
+
+    public String getFailureCategory() {
+        if (this.caesar.getFailureCategory() != null) {
+            return this.caesar.getFailureCategory();
+        } else {
+            return "No category found!";
+        }
+    }
+
+    public List<Error> getErrors() {
+        return this.caesar.getErrors();
     }
 }
--- caesar-intellij-plugin/src/main/java/ch/scheitlin/alex/intellij/plugins/services/helpers/IntelliJHelper.java
+++ caesar-intellij-plugin/src/main/java/ch/scheitlin/alex/intellij/plugins/services/helpers/IntelliJHelper.java
@@ -1,20 +1,36 @@
 package ch.scheitlin.alex.intellij.plugins.services.helpers;
 
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindowManager;
+
+import java.io.File;
 
 public class IntelliJHelper {
     public static VirtualFile getProjectDirectoryFile(Project project) {
         return project.getBaseDir();
     }
+
     public static String getProjectPath(Project project) {
         return getProjectDirectoryFile(project).getPath();
+    }
+
+    public static void reloadProjectFiles(Project project) {
+        getProjectDirectoryFile(project).refresh(true, true);
     }
 
     public static void showToolWindow(Project project, String toolWindowId) {
         ToolWindowManager manager = ToolWindowManager.getInstance(project);
         com.intellij.openapi.wm.ToolWindow toolWindow = manager.getToolWindow(toolWindowId);
         toolWindow.show(null);
+    }
+
+    public static void openFile(Project project, String filePath, int lineNumber, int columnNumber) {
+        File file = new File(filePath);
+        VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(file);
+        OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, virtualFile, lineNumber, columnNumber);
+        openFileDescriptor.navigate(true);
     }
 }
