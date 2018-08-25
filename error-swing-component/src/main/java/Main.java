0b3f0675122ee9db8c2f68c6f4163c0683641f3a
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-25 14:44:09 +0200

    Make error label collapsible

--- error-swing-component/src/main/java/Main.java
+++ error-swing-component/src/main/java/Main.java
@@ -10,28 +10,35 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Error> errors = getDummyErrors_no_path();
-
-        // create new error swing component
-        ErrorPanel errorComponent = new ErrorPanel(errors.get(0), "Show");
-
-        // create action for action button of error component
-        final ErrorPanel that = errorComponent;
-        ActionListener actionListener = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Opening " + that.getError().getPath() + "/" + that.getError().getFile() + "...");
-            }
-        };
-        errorComponent.addButtonAction(actionListener);
-
-        // create frame and add error components
+        // create frame
         JFrame frame = new JFrame();
         frame.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = 0;
-        frame.add(errorComponent, c);
-        frame.setSize(new Dimension(800,800));
+        frame.setSize(new Dimension(200, 200));
+
+        // create new error swing components
+        List<Error> errors = getDummyErrors();
+        ErrorPanel[] errorComponents = new ErrorPanel[getDummyErrors().size()];
+        for (int i = 0; i < errors.size(); i++) {
+            errorComponents[i] = new ErrorPanel(errors.get(i), "Show");
+
+            // create action for action button of error component
+            final ErrorPanel that = errorComponents[i];
+            ActionListener actionListener = new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Opening " + that.getError().getPath() + "/" + that.getError().getFile() + "...");
+                }
+            };
+            errorComponents[i].addButtonAction(actionListener);
+
+            // create frame and add error components
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridy = i;
+            c.insets = new Insets(10, 0, 10, 0);
+            c.weightx = 1.0;
+            frame.add(errorComponents[i], c);
+        }
 
         // set look and feel of frame
         try {
@@ -41,13 +48,15 @@ public class Main {
                     break;
                 }
             }
-        } catch (Exception ex) {}
+        } catch (Exception ex) {
+        }
 
         // show frame
         frame.setVisible(true);
     }
 
     public static List<Error> getDummyErrors() {
+        // normal error
         String path1 = "C:/users/alex/desktop";
         String file1 = "Test.java";
         int line1 = 1;
@@ -55,22 +64,26 @@ public class Main {
         String message1 = "Error message";
         Error error1 = new Error(path1, file1, line1, column1, message1);
 
+        // error with no path
+        String path2 = null;
+        String file2 = "Test.java";
+        int line2 = 1;
+        int column2 = 2;
+        String message2 = "Error message";
+        Error error2 = new Error(path2, file2, line2, column2, message2);
+
+        // error with very long message and multiple lines
+        String path3 = "C:/users/alex/desktop";
+        String file3 = "Test.java";
+        int line3 = 1;
+        int column3 = 2;
+        String message3 = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.\nStet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+        Error error3 = new Error(path3, file3, line3, column3, message3);
+
         ArrayList errors = new ArrayList<Error>();
         errors.add(error1);
-
-        return errors;
-    }
-
-    public static List<Error> getDummyErrors_no_path() {
-        String path1 = null;
-        String file1 = "Test.java";
-        int line1 = 1;
-        int column1 = 2;
-        String message1 = "Error message";
-        Error error1 = new Error(path1, file1, line1, column1, message1);
-
-        ArrayList errors = new ArrayList<Error>();
-        errors.add(error1);
+        errors.add(error2);
+        errors.add(error3);
 
         return errors;
     }
--- error-swing-component/src/main/java/ch/scheitlin/alex/build/swing/ErrorPanel.java
+++ error-swing-component/src/main/java/ch/scheitlin/alex/build/swing/ErrorPanel.java
@@ -2,13 +2,11 @@ package ch.scheitlin.alex.build.swing;
 
 import ch.scheitlin.alex.build.model.Error;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import javax.swing.*;
 
 public class ErrorPanel extends JPanel {
     // data
@@ -17,8 +15,18 @@ public class ErrorPanel extends JPanel {
 
     // components
     private JLabel labelPath;
-    private JLabel labelMessage;
+    private JLabel labelShowMore;
+    private JTextPane labelMessage;
     private JButton buttonAction;
+
+    // appearance settings
+    private boolean showMore = false;
+
+    // appearance constants
+    private final String SHOW_MORE_TEXT = "&#x25BC"; // ▼
+    private final String SHOW_LESS_TEXT = "&#x25B2";  // ▲
+    private final String SHOW_MORE_TEXT_ENABLED_COLOR = "1E90FF";
+    private final String SHOW_MORE_DISABLED_COLOR = "C0C0C0";
 
     public ErrorPanel(Error error, String actionButtonText) {
         // set data variables
@@ -38,21 +46,35 @@ public class ErrorPanel extends JPanel {
 
         // initialize label showing the path and file name where the error occurred
         this.labelPath = initPathLabel(errorPath, errorFile, errorLine, errorColumn);
-        c.anchor = GridBagConstraints.LINE_START;
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
         c.insets = new Insets(0, 0, 0, 0);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridheight = 1;
+        c.gridwidth = 1;
         c.gridx = 0;
         c.gridy = 0;
         c.weightx = 1.0;
         this.add(this.labelPath, c);
 
+        // initialize label to show more or less of the error message
+        this.labelShowMore = initShowMoreLabel();
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+        c.insets = new Insets(0, 5, 0, 0);
+        c.fill = GridBagConstraints.NONE;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.weightx = 0.0;
+        this.add(this.labelShowMore, c);
+
         // initialize label showing the error message
         this.labelMessage = initMessageLabel(errorMessage);
-        c.anchor = GridBagConstraints.LINE_START;
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
         c.insets = new Insets(0, 0, 0, 0);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridheight = 1;
+        c.gridwidth = 2;
         c.gridx = 0;
         c.gridy = 1;
         c.weightx = 1.0;
@@ -60,14 +82,50 @@ public class ErrorPanel extends JPanel {
 
         // initialize button to start action
         this.buttonAction = initActionButton(this.actionButtonText);
-        c.anchor = GridBagConstraints.LINE_START;
-        c.insets = new Insets(0, 20, 0, 0);
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.insets = new Insets(0, 5, 0, 0);
         c.fill = GridBagConstraints.NONE;
         c.gridheight = 2;
-        c.gridx = 1;
+        c.gridwidth = 1;
+        c.gridx = 2;
         c.gridy = 0;
         c.weightx = 0.0;
         this.add(this.buttonAction, c);
+
+        // resize behaviour
+        this.addComponentListener(new ComponentListener() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                if (showMore) {
+                    resizeMessageLabel();
+                } else {
+                    setMessageLabelText();
+                }
+            }
+
+            @Override
+            public void componentMoved(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentShown(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentHidden(ComponentEvent e) {
+
+            }
+        });
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                String labelText = showMore ? SHOW_LESS_TEXT : SHOW_MORE_TEXT;
+                String labelColor = SHOW_MORE_TEXT_ENABLED_COLOR;//isMessageComplete() ? SHOW_MORE_TEXT_ENABLED_COLOR : SHOW_MORE_DISABLED_COLOR;
+                String labelContent = "<html><span style='color: " + labelColor + ";'>" + labelText + "</span></html>";
+                labelShowMore.setText(labelContent);
+            }
+        });
     }
 
     public Error getError() {
@@ -91,9 +149,47 @@ public class ErrorPanel extends JPanel {
         return label;
     }
 
-    private JLabel initMessageLabel(String message) {
+    private JLabel initShowMoreLabel() {
         JLabel label = new JLabel();
+
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                toggleShowMore();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                labelShowMore.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                labelShowMore.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+        });
+
+        return label;
+    }
+
+    private JTextPane initMessageLabel(String message) {
+        JTextPane label = new JTextPane();
+        label.setEditable(false);
+        label.setCursor(null);
+        label.setOpaque(false);
+        label.setFocusable(false);
+
         label.setText(message);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                resizeMessageLabel();
+            }
+        });
 
         return label;
     }
@@ -103,6 +199,76 @@ public class ErrorPanel extends JPanel {
         button.setText(text);
 
         return button;
+    }
+
+    private void toggleShowMore() {
+        // label show more settings
+        showMore = !showMore;
+
+        String labelText = showMore ? this.SHOW_LESS_TEXT : this.SHOW_MORE_TEXT;
+        //String labelColor = isMessageComplete() ? this.SHOW_MORE_TEXT_ENABLED_COLOR : this.SHOW_MORE_DISABLED_COLOR;
+        String labelContent = "<html><span style='color: " + this.SHOW_MORE_TEXT_ENABLED_COLOR + ";'>" + labelText + "</span></html>";
+
+        labelShowMore.setText(labelContent);
+
+        // label error message settings
+        resizeMessageLabel();
+    }
+
+    private void resizeMessageLabel() {
+        // set size
+        int width;
+        int height;
+
+        if (!this.showMore) {
+            width = this.labelMessage.getWidth();
+            height = 22;
+        } else {
+            this.labelMessage.setText(this.error.getMessage());
+            width = this.labelMessage.getWidth();
+            height = this.labelMessage.getPreferredSize().height;
+        }
+
+        Dimension newSize = new Dimension(width, height);
+        this.labelMessage.setMinimumSize(newSize);
+        this.labelMessage.setMaximumSize(newSize);
+    }
+
+    private void setMessageLabelText() {
+        // set three dots at the end if label to small
+        String labelText = error.getMessage();
+        Font labelFont = labelMessage.getFont();
+        int labelWidth = labelMessage.getWidth();
+
+        if (isMessageComplete()) {
+            int start = 8;
+            for (int i = start; i < labelText.toCharArray().length; i++) {
+                String currentText = labelText.substring(0, i);
+                int currentTextWidth = getTextWidth(currentText, labelFont);
+                if (currentTextWidth > labelWidth) {
+                    String text = labelText.substring(0, i - start) + " ...";
+                    this.labelMessage.setText(text);
+                    break;
+                }
+            }
+        } else {
+            this.labelMessage.setText(labelText);
+        }
+    }
+
+    private boolean isMessageComplete() {
+        String text = error.getMessage();
+        Font font = labelMessage.getFont();
+        int textWidth = getTextWidth(text, font);
+        int labelWidth = labelMessage.getWidth();
+
+        return textWidth > labelWidth;
+    }
+
+    private int getTextWidth(String text, Font font) {
+        AffineTransform affinetransform = new AffineTransform();
+        FontRenderContext frc = new FontRenderContext(affinetransform, true, true);
+        return (int) (font.getStringBounds(text, frc).getWidth());
     }
 
     public void addButtonAction(ActionListener actionListener) {
